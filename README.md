572b334caec2865a040fb2bc32df908c5aebc8bf
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-08-02 15:00:52 -0300

    Parse polish notation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # clisp
-A Lisp interpreter written in C99
+A weekend implementation of [Daniel Holden](https://github.com/orangeduck)'s ["Build Your Own Lisp"](http://www.buildyourownlisp.com/), written in C99.
---
+++ main.c
@@ -0,0 +1,153 @@
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "mpc.h"
+
+#ifdef _WIN32
+#include <string.h>
+
+// @FIXME handle input better (using a fixed size buffer,
+// even if large, is error-prone.. although common in C)
+#define BUFFER_SIZE 2048
+
+static char buffer[BUFFER_SIZE];
+
+char *readline(char *prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, BUFFER_SIZE, stdin);
+
+    // @FIXME use strncpy and pass a max size to strlen
+    char *cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+
+    return cpy;
+}
+
+void add_history(char *unused) {}
+
+#else // *nix or macOS
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+typedef enum { LVAL_NUM, LVAL_ERR } LVAL_TYPE;
+typedef enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM } LERR_TYPE;
+
+// A Lispy value, which is either "some thing" or an error.
+typedef struct lval {
+    LVAL_TYPE   type;
+    long        num;
+    LERR_TYPE   err;
+} lval;
+
+lval lval_num(const long x) {
+    return (lval) { .type = LVAL_NUM, .num = x };
+}
+
+lval lval_err(const LERR_TYPE x) {
+    return (lval) { .type = LVAL_ERR, .err = x };
+}
+
+void lval_print(const lval v) {
+    switch (v.type) {
+        case LVAL_NUM: printf("%li", v.num); break;
+
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) printf("Error: Division By Zero!");
+            if (v.err == LERR_BAD_OP) printf("Error: Invalid Operator!");
+            if (v.err == LERR_BAD_NUM) printf("Error: Invalid Number!");
+            break;
+    }
+}
+
+void lval_println(const lval v) {
+    lval_print(v); putchar('\n');
+}
+
+lval eval_op(const lval x, const char *op, const lval y) {
+    if (x.type == LVAL_ERR) return x;
+    if (y.type == LVAL_ERR) return y;
+
+    assert(x.type == LVAL_NUM && y.type == LVAL_NUM);
+
+    if (strcmp(op, "+") == 0) return lval_num(x.num + y.num);
+    if (strcmp(op, "-") == 0) return lval_num(x.num - y.num);
+    if (strcmp(op, "*") == 0) return lval_num(x.num * y.num);
+    if (strcmp(op, "/") == 0) {
+        return y.num == 0
+            ? lval_err(LERR_DIV_ZERO)
+            : lval_num(x.num / y.num);
+    }
+
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(const mpc_ast_t *t) {
+    // If tagged as number, return it directly.
+    if (strstr(t->tag, "number")) {
+        errno = 0;
+        const long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE
+            ? lval_num(x)
+            : lval_err(LERR_BAD_NUM);
+    }
+
+    // The operator is always the second child.
+    const char *op = t->children[1]->contents;
+
+    // Store the third child in `x`, iterate through
+    // the remaining children, and combine the result.
+    lval x = eval(t->children[2]);
+
+    for (int i = 3; strstr(t->children[i]->tag, "expr"); ++i)
+        x = eval_op(x, op, eval(t->children[i]));
+
+    return x;
+}
+
+int main(int argc, const char *argv[]) {
+    mpc_parser_t *Number   = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr     = mpc_new("expr");
+    mpc_parser_t *Lispy    = mpc_new("lispy");
+
+    mpca_lang(
+        MPCA_LANG_DEFAULT,
+        "                                                       \
+            number   : /-?[0-9]+/ ;                             \
+            operator : '+' | '-' | '*' | '/' ;                  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+            lispy    : /^/ <operator> <expr>+ /$/ ;             \
+        ",
+        Number, Operator, Expr, Lispy
+    );
+
+    puts("Lispy Version 0.6.6.6");
+    puts("Press Ctrl+C to exit\n");
+
+    while (true) {
+        char *input = readline("lispy> ");
+        add_history(input);
+
+        // Parse the user input.
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            lval_println(eval(r.output));
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
+    }
+
+    // Undefine and delete parsers.
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
+    return 0;
+}
